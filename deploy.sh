ca362f7973fb5061adb54c12feaf105e0df3222a
Damir Mehic <damir.mehic@legal.one>
2020-12-12 18:37:36 +0100

    Try to fix the 502 on GCloud

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t dado83/multi-client:latest -t dado83/multi-client:$SHA -f ./client/Dockerfile ./client
+#docker build -t dado83/multi-client:latest -t dado83/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t dado83/multi-server:latest -t dado83/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t dado83/multi-worker:latest -t dado83/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push dado83/multi-client:latest
+#docker push dado83/multi-client:latest
 docker push dado83/multi-server:latest
 docker push dado83/multi-worker:latest
 
-docker push dado83/multi-client:$SHA
+#docker push dado83/multi-client:$SHA
 docker push dado83/multi-server:$SHA
 docker push dado83/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=dado83/multi-server:$SHA
-kubectl set image deployments/client-deployment client=dado83/multi-client:$SHA
+kubectl set image deployments/client-deployment client=stephengrider/multi-client:latest    
 kubectl set image deployments/worker-deployment worker=dado83/multi-worker:$SHA
